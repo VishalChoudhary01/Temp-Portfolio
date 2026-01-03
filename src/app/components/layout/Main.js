ff61ff0962afcc3dd10794830fb7663df3f0c6ec
Vishal <vishal.bhumca21@gmail.com>
2026-01-03 19:36:28 +0530

    main component removed lazy

--- src/app/components/layout/Main.js
+++ src/app/components/layout/Main.js
@@ -3,7 +3,7 @@ import {Education,Skills,ProjectsCarousel,WorkExperience} from '../../components
 
 const Main = () => {
   return (
-    <div lazy="true" className='w-full md:mt-96 mt-[700px] '>
+    <div  className='w-full md:mt-96 mt-[700px] '>
         <Education/>
         <WorkExperience/>
         <Skills/>
